2c230d2fb182cfcc579834b6c341cee6572f9efd
Vinícius Garcia <vingarcia00@gmail.com>
2016-01-23 02:58:29 -0200

    Simplify `effects/ai.js`

--- effects/ai.js
+++ effects/ai.js
@@ -2,11 +2,8 @@ require('./effect.js')
 
 AI = Effect.extend({
   init : function(func) {
-    if(!this.instanceof(Time))
-      this.apply(Time)
-
-    if(!this.instanceof(Space))
-      this.apply(Space)
+    this.apply(Time)
+    this.apply(Space)
 
     this.add( func )
   }
